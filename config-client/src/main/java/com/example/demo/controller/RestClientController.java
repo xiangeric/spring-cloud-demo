20c2c8012f9a2249923e30ab1d2db01bb8a6ee6e
xiangeric <chenqx_gz@126.com>
2020-03-08 17:07:29 +0800

    Add Spring cloud config module demo: config-client, config-server

---
+++ config-client/src/main/java/com/example/demo/controller/RestClientController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/rest")
+public class RestClientController {
+
+    @Value("${server.port}")
+    private Integer serverPort;
+
+    @Value("${spring.profiles}")
+    private String springProfiles;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @GetMapping
+    public Map<String,String> applicationConf(){
+        Map<String,String> data = new HashMap<>();
+        data.put("server.port",String.valueOf(serverPort));
+        data.put("spring.profiles",springProfiles);
+        data.put("spring.application.name",applicationName);
+        return data;
+    }
+}
