d219688eef96daf91d5e77e788c072acc94214b7
xiangeric <chenqx_gz@126.com>
2020-03-11 23:00:08 +0800

    update configuration for refresh the config without restart the springboot application

--- config-client/src/main/java/com/example/demo/controller/RestClientController.java
+++ config-client/src/main/java/com/example/demo/controller/RestClientController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/rest")
+@RefreshScope
 public class RestClientController {
 
     @Value("${server.port}")
@@ -21,12 +23,16 @@ public class RestClientController {
     @Value("${spring.application.name}")
     private String applicationName;
 
+    @Value("${custom.property}")
+    private String customProperty;
+
     @GetMapping
     public Map<String,String> applicationConf(){
         Map<String,String> data = new HashMap<>();
         data.put("server.port",String.valueOf(serverPort));
         data.put("spring.profiles",springProfiles);
         data.put("spring.application.name",applicationName);
+        data.put("custom.property",customProperty);
         return data;
     }
 }
