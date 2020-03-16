eff73579a6e5bcf136b8f134ffc4f29e2e638c52
xiangeric <chenqx_gz@126.com>
2020-03-16 23:34:28 +0800

    add binder for copying file with RabbitMQ

---
+++ common/src/main/java/org/example/constants/FileConstants.java
@@ -0,0 +1,11 @@
+package org.example.constants;
+
+public class FileConstants {
+
+    public final static String GET = "get";
+
+    public final static String BACK = "back";
+
+    public final static String FILE_NAME_HEADER = "FILE_NAME";
+
+}
