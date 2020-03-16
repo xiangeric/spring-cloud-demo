2d8617614c272884f52855669239db3016749050
xiangeric <chenqx_gz@126.com>
2020-03-16 23:33:03 +0800

    (1) add test code for get MessageChannel bean from ApplicationContext

--- stream-provider/src/test/java/StreamProviderTest.java
+++ stream-provider/src/test/java/StreamProviderTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.UUID;
@@ -15,6 +17,27 @@ public class StreamProviderTest {
 
     @Autowired
     private MessageSender messageSender;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+
+    @Test
+    public void getChannel(){
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        Object bean;
+        for(String name:beanDefinitionNames){
+             bean = applicationContext.getBean(name);
+             if(bean instanceof MessageChannel){
+                 System.out.println(name+":"+bean);
+             }
+
+        }
+
+    }
+
+
+
 
 
     @Test
